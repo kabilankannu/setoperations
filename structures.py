4c2df01f2d3f8cf81655b2f7ee67318e2e5e7c9b
kabilankannu <85838280+kabilankannu@users.noreply.github.com>
2021-08-27 20:01:57 +0530

    Add files via upload

---
+++ structures.py
@@ -0,0 +1,6 @@
+n={1,2,3,4,5,6,7,8}
+y={1,3,5,7,9,10,12}
+print("The intresection is:",n.intersection(y))
+print("The union is:",n.union(y))
+print("The difference is:",n.difference(y))
+print("The symmetric difference is:",n.symmetric_difference(y))
